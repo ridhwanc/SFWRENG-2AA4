563973d7409c7133c989c5c295f7999710059b99
Ridhwan A. Chowdhury <ridhwan.c@gmail.com>
2019-07-27 11:02:23 -0400

    Add files via upload

---
+++ CardStack.h
@@ -0,0 +1,17 @@
+/**
+ * \file CardStack.h
+ * \brief Defines CardStackT as a type for implementation
+ * \author Ridhwan Chowdhury, MacID: chowdr11
+ * \date March 25, 2019
+*/
+#ifndef A3_CARD_STACK_H_
+#define A3_CARD_STACK_H_
+
+#include "CardTypes.h"
+#include "Stack.h"
+/**
+ * \brief Defines the stack of CardT as a the type CardStackT used in implementation
+ */
+typedef Stack<CardT> CardStackT;
+
+#endif
